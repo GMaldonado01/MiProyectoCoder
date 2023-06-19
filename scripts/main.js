865f0adf6060db7bea0e092a4fb4dab6e9d8e4ed
Gaston Maldonado <gastonmaldonado@MacBook-Air-de-Gaston.local>
2023-06-19 13:20:26 -0300

    incorpore primer estructura js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
+let edad
+let repetir
+do {
+    edad = prompt('Ingrese su edad')
+
+    if (edad >= 18) {
+        alert('Sos mayor de edad. Podes ingresar')
+        let kilosIngresados = prompt('Cuantos kilos de torta quieres comprar?')
+
+        function precio(kilos) {
+            return 1000 * kilos
+        }
+
+        alert('el valor de tu torta es $ ' + precio(kilosIngresados))
+        repetir = prompt('Quiere hacer otra compra?')
+
+
+
+    } else {
+        alert('Sos menor de edad. No podes ingresar')
+
+
+    }
+} while (edad < 18 || repetir == 'si')
+
+
+
 
