bb13ce42313cd452be79955d3954e15399b2d8ab
Gaston Maldonado <gastonmaldonado@MacBook-Air-de-Gaston.local>
2023-07-08 10:38:43 -0300

    agregue javascript con arrays al proyecto

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,98 @@
-let edad
-let repetir
-do {
-    edad = prompt('Ingrese su edad')
 
-    if (edad >= 18) {
-        alert('Sos mayor de edad. Podes ingresar')
-        let kilosIngresados = prompt('Cuantos kilos de torta quieres comprar?')
+// do {
+//     edad = prompt('Ingrese su edad')
 
-        function precio(kilos) {
-            return 1000 * kilos
+
+//     if (edad >= 18) {
+//         alert('Sos mayor de edad. Podes ingresar')
+//     } else (edad < 18 || repetir == 'si')
+// }
+
+// var alfajores = [
+//     { nombre: "Alfajor de Maicena", precio: 1000 },
+//     { nombre: "Alfajor Marplatense", precio: 3600 },
+//     { nombre: "Alfalemon", precio: 3600 },
+//     { nombre: "Alfanevado", precio: 3600 },
+//     { nombre: "Alfacafe", precio: 3600 },
+//     { nombre: "Alfajor Santafesino", precio: 3000 },
+//     { nombre: "Alfajores de Nuez", precio: 3600 },
+//     { nombre: "Alfajor Rafaelinos", precio: 3000 },
+//     { nombre: "Alfacoco", precio: 3600 }
+// ];
+
+// var nombresAlfajores = alfajores.map(function (alfajor) {
+//     return alfajor.nombre;
+// });
+
+// var mensaje = '¿Cuál de estos alfajores quieres comprar?\n\n' + nombresAlfajores.join(', ');
+// var eleccion = prompt(mensaje);
+// console.log('Has elegido:', eleccion);
+
+// var continuarComprando = true;
+
+// do {
+//     var nombresAlfajores = alfajores.map(function (alfajor) {
+//         return alfajor.nombre;
+//     });
+
+//     var mensaje = '¿Cuál de estos alfajores quieres comprar?\n\n' + nombresAlfajores.join(', ');
+//     var eleccion = prompt(mensaje);
+//     console.log('Has elegido:', eleccion);
+
+//     var respuesta = prompt('¿Deseas hacer otra compra? (Sí/No)');
+//     if (respuesta !== 'Sí' && respuesta !== 'sí' && respuesta !== 'SI') {
+//         continuarComprando = false;
+//     }
+// } while (continuarComprando);
+
+var alfajores = [
+    { nombre: "Alfajor de Maicena", precio: 1000 },
+    { nombre: "Alfajor Marplatense", precio: 3600 },
+    { nombre: "Alfalemon", precio: 3600 },
+    { nombre: "Alfanevado", precio: 3600 },
+    { nombre: "Alfacafe", precio: 3600 },
+    { nombre: "Alfajor Santafesino", precio: 3000 },
+    { nombre: "Alfajores de Nuez", precio: 3600 },
+    { nombre: "Alfajor Rafaelinos", precio: 3000 },
+    { nombre: "Alfacoco", precio: 3600 }
+];
+
+var total = 0;
+var deseaComprarMas = true;
+
+// Mayor de 18?
+var edad = prompt("Ingrese su edad:");
+if (parseInt(edad) < 18) {
+    alert("Lo sentimos, debes ser mayor de 18 años para comprar alfajores.");
+} else {
+    while (deseaComprarMas) {
+        var opciones = "";
+
+
+        // Mostrar el array para que haga su eleccion
+        for (var i = 0; i < alfajores.length; i++) {
+            opciones += (i + 1) + ". " + alfajores[i].nombre + "\n";
         }
 
-        alert('el valor de tu torta es $ ' + precio(kilosIngresados))
-        repetir = prompt('Quiere hacer otra compra?')
+        // Seleccion del usuario 
+        var eleccion = prompt("Seleccione el número del alfajor que desea comprar:\n\n" + opciones);
 
+        // Validar la elección
+        eleccion = parseInt(eleccion);
+        if (!isNaN(eleccion) && eleccion >= 1 && eleccion <= alfajores.length) {
+            var alfajorSeleccionado = alfajores[eleccion - 1];
+            total += alfajorSeleccionado.precio;
 
+            // Mostrar el nombre del alfajor seleccionado y la suma de los precios
+            alert("Ha seleccionado: " + alfajorSeleccionado.nombre + "\nTotal acumulado: " + total);
+        } else {
+            alert("La opción seleccionada es inválida.");
+        }
 
-    } else {
-        alert('Sos menor de edad. No podes ingresar')
-
-
+        // Preguntar si quiere comprar más
+        deseaComprarMas = confirm("¿Desea comprar otro alfajor?");
     }
-} while (edad < 18 || repetir == 'si')
 
-
-
-
+    // Mostrar el total final
+    alert("El total a pagar es: " + total);
+}
